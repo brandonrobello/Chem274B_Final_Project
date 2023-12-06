08e677811e67d1403ef708aa365b9d09457292d5
Curtis-Wu <eedeverdeen@gmail.com>
2023-12-05 18:47:21 -0800

    Updated majority rule message passing function

--- Include/myCA.h
+++ Include/myCA.h
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <map>
 #include <vector>
 
 using namespace std;
 
-class CelluarAutomata
+class CellularAutomata
 {
     public:
         vector<vector<vector<int>>> grid;    // 3D grid structure that stores double data type
@@ -15,37 +16,40 @@ class CelluarAutomata
         int dim2;               // Number of columns for CA
         int n_feats;            // Number of features for each cell
         int message_radius;     // Message passing radius
-        int rule_type;          // What is this rule type?
+        int rule_type;          // Rule type of the cellular automata
 
-        CelluarAutomata();
-        ~CelluarAutomata();
+        CellularAutomata();
+        ~CellularAutomata();
 
 };
+
 #define VONNEUMANN  1
 #define MOORE       2
 #define STATIC      3
 #define PERIODIC    4
+#define MAJORITY    5
+#define PARITY      6
 
 // Dimension and initial setup for the cellular automaton
-int CA_setup_dimension(CelluarAutomata &CA, int ndims, int dim1, int dim2, int n_feats);
+int CA_setup_dimension(CellularAutomata &CA, int ndims, int dim1, int dim2, int n_feats);
 
 // Neighborhood type set up for the cellular automaton
-int CA_setup_neighborhood(CelluarAutomata &CA,int neigh_type);
+int CA_setup_neighborhood(CellularAutomata &CA,int neigh_type);
 
 // Boundary type setup for the cellular automaton
-int CA_setup_boundtype(CelluarAutomata &CA,int bound_type, int radius);
+int CA_setup_boundtype(CellularAutomata &CA,int bound_type, int radius);
 
 // Cell states setup for the cellular automaton
-int CA_setup_cell_states(CelluarAutomata &CA, vector<vector<int>> coords, int nth_feature, int val);
+int CA_setup_cell_states(CellularAutomata &CA, vector<vector<int>> coords, int nth_feature, int val);
 
 // Probability x of cell entering state x when initialized
-int CA_init_cond(CelluarAutomata CA, vector<int> coord, int nth_feature, double prob, int val);
+int CA_init_cond(CellularAutomata &CA, vector<int> coord, int nth_feature, double prob, int val);
 
 // Set up rule for the cells
-int CA_setup_rule(CelluarAutomata CA,int rule_type);
+int CA_setup_rule(CellularAutomata &CA,int rule_type);
 
 // Set up rule for the cells with probability
-int CA_setup_rule_wprob(CelluarAutomata CA,int rule_type, double prob);
+int CA_setup_rule_wprob(CellularAutomata &CA,int rule_type, double prob);
 
-// Rewrite probability x of cell entering state x when initialized
-int CA_init_cond_rewrite(CelluarAutomata CA,int x_state, double probability);
+// Message passing update function for the majority rule
+int majority_rule_update(CellularAutomata &CA, vector<int> coord, int nth_feature);
--- src/CellularAutomata.cpp
+++ src/CellularAutomata.cpp
@@ -2,11 +2,11 @@
 #include "myCA.h"
 
 // Consturctor and Destructor for the cellular automata
-CelluarAutomata::CelluarAutomata(){}
-CelluarAutomata::~CelluarAutomata(){}
+CellularAutomata::CellularAutomata(){}
+CellularAutomata::~CellularAutomata(){}
 
 // Dimension and initial setup for the cellular automaton, reserves space for the cellular automaton vectors
-int CA_setup_dimension(CelluarAutomata &CA, int ndims, int dim1, int dim2, int n_feats)
+int CA_setup_dimension(CellularAutomata &CA, int ndims, int dim1, int dim2, int n_feats)
 {
     if ((ndims != 1 && ndims != 2) || dim1 <= 0 || (ndims == 2 && dim2 <= 0) || n_feats <= 0) {
         cout<<"Invalid dimensions !"<<endl;
@@ -37,7 +37,7 @@ int CA_setup_dimension(CelluarAutomata &CA, int ndims, int dim1, int dim2, int n
 }
 
 // Neighborhood type set up for the cellular automaton
-int CA_setup_neighborhood(CelluarAutomata &CA,int neighbor_type)
+int CA_setup_neighborhood(CellularAutomata &CA,int neighbor_type)
 {
      // Von Neumann neighborhood type
     if (neighbor_type == VONNEUMANN) {
@@ -59,7 +59,7 @@ int CA_setup_neighborhood(CelluarAutomata &CA,int neighbor_type)
 }
 
 // Boundary type setup for the cellular automaton
-int CA_setup_boundtype(CelluarAutomata &CA,int bound_type, int radius)
+int CA_setup_boundtype(CellularAutomata &CA,int bound_type, int radius)
 {
     if (radius <= 0) {
         cout<<"Message passing radius has to be a positive value!"<<endl;
@@ -88,7 +88,7 @@ int CA_setup_boundtype(CelluarAutomata &CA,int bound_type, int radius)
 // Cell states setup for the cellular automaton
 // nth_feature: indicates the feature index to update the value with
 // val: value that will be used to update the cells
-int CA_setup_cell_states(CelluarAutomata &CA, vector<vector<int>> coords, int nth_feature, int val)
+int CA_setup_cell_states(CellularAutomata &CA, vector<vector<int>> coords, int nth_feature, int val)
 {
     // Ssanity check for feature index value
     if (nth_feature >= CA.n_feats || nth_feature < 0) {
@@ -110,7 +110,7 @@ int CA_setup_cell_states(CelluarAutomata &CA, vector<vector<int>> coords, int nt
 }
 
 // Probability x of cell entering state x when initialized
-int CA_init_cond(CelluarAutomata CA, vector<int> coord, int nth_feature, double prob, int val)
+int CA_init_cond(CellularAutomata &CA, vector<int> coord, int nth_feature, double prob, int val)
 {
     // Ssanity check for feature index value
     if (nth_feature >= CA.n_feats || nth_feature < 0) {
@@ -137,15 +137,25 @@ int CA_init_cond(CelluarAutomata CA, vector<int> coord, int nth_feature, double 
 }
 
 // Set up rule for the cells
-int CA_setup_rule(CelluarAutomata CA,int rule_type)
+int CA_setup_rule(CellularAutomata &CA, int rule_type)
 {
+    // Invalid rule type
+    if (rule_type != MAJORITY && rule_type != PARITY) {
+        cout<<"Invalid rule type!"<<endl;
+        return -1;
+    }
     CA.rule_type = rule_type;
     return 0;
 }
 
 // Set up rule for the cells with probability
-int CA_setup_rule_wprob(CelluarAutomata CA,int rule_type, double prob)
+int CA_setup_rule_wprob(CellularAutomata &CA, int rule_type, double prob)
 {
+    // Invalid rule type
+    if (rule_type != MAJORITY && rule_type != PARITY) {
+        cout<<"Invalid rule type!"<<endl;
+        return -1;
+    }
     srand(static_cast<unsigned int>(time(0)));
     // Generate a random number between 0 and 1
     double randomValue = static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
@@ -153,4 +163,71 @@ int CA_setup_rule_wprob(CelluarAutomata CA,int rule_type, double prob)
         CA.rule_type = rule_type;
     }
     return 0;
+}
+
+// Message passing update function for the majority rule
+int majority_rule_update(CellularAutomata &CA, vector<int> coord, int nth_feature)
+{
+    // Grab the passed in coordiante and perform sanity check
+    int x = coord[0];
+    int y = coord[1];
+
+    if (x >= CA.dim1 || x < 0 || y >= CA.dim2 || y < 0 || nth_feature < 0) {
+        cout<<"Invalid coordinate or feature index!"<<endl;
+        return -1;
+    }
+
+    int radius = CA.message_radius;
+    // Map for storing frequency
+    map<int, int> frequencyMap;
+
+    for (int d = 1; d <= radius; ++d) {
+        // Get the coordinate values for the 4 directions
+        int left = x - d;
+        int right = x + d;
+        int top = y - d;
+        int bottom = y + d;
+
+        if (CA.bound_type == PERIODIC) {
+            // Modify the 4 values for periodic boundary type
+            left = (left + CA.dim1) % CA.dim1;
+            right = (right + CA.dim1) % CA.dim1;
+            top = (top + CA.dim2) % CA.dim2;
+            bottom = (bottom + CA.dim2) % CA.dim2;
+        }
+        // Create a vector storing to be added coordinates
+        vector<vector<int>> to_be_added;
+        if (CA.neighbor_type == VONNEUMANN) {
+            // Only add north, south, west, east for Von Neumann neighbor rule
+            to_be_added = {{left, y}, {right, y}, {x, top}, {x, bottom}};
+        }
+        else {
+            // Add all adjacent cells for Moore neighbor rule
+            to_be_added = {{left, y}, {right, y}, {x, top}, {x, bottom}, {left, top}, 
+                            {left, bottom}, {right, top}, {right, bottom}};
+        }
+        // Loop through all the coordinates in the to_be_added vector, and record the frequency
+        for (const auto &coord : to_be_added) {
+            
+            int x_coord = coord[0];
+            int y_coord = coord[1];
+            
+            if (x_coord >= CA.dim1 || x_coord < 0 || y_coord >= CA.dim2 || y_coord < 0) {
+                continue; // Continue if coordinate is out of bounds
+            }
+            // Add one count to that specific value
+            frequencyMap[CA.grid[x_coord][y_coord][nth_feature]]++;
+        }
+    }
+
+    int maxFrequencyValue = 0;
+    int maxFrequency = 0;
+    for (const auto &entry : frequencyMap) {
+        if (entry.second > maxFrequency) {
+            maxFrequency = entry.second;
+            maxFrequencyValue = entry.first;
+        }
+    }
+    // Update cell value the highest frequency value
+    CA.grid[x][y][nth_feature] = maxFrequencyValue;
 }
